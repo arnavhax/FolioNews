b4c0e2f723734b05fcf681c9b4e57ffa3ba17fcc
Arnav Balpande <arnavbalpande14@gmail.com>
2024-07-08 14:46:16 +0530

    delete option updated

--- main/static/main/script.js
+++ main/static/main/script.js
@@ -1,4 +1,4 @@
-window.scrollTo(0,500);
+
 document.getElementById('saveStockForm').addEventListener('submit', function(event) {
     console.log("event listner hit")
     event.preventDefault(); // Prevent the default form submission
@@ -25,33 +25,7 @@ document.getElementById('saveStockForm').addEventListener('submit', function(eve
     });
 });
 
-document.querySelectorAll('.deleteStockForm').forEach(function(form) {
-    form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
-        var formData = new FormData(form);
 
-        // Perform AJAX request to submit the deleteStock form
-        fetch(form.action, {
-            method: form.method,
-            body: formData,
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest'
-            }
-        }).then(response => {
-            if (response.ok) {
-                // Form submitted successfully, now submit the update form
-                setTimeout(function() {
-                    document.getElementById('updateForm').submit();
-                }, 1000);
-            } else {
-                // Handle the error response if needed
-                console.error('Failed to submit deleteStock form.');
-            }
-        }).catch(error => {
-            console.error('Error:', error);
-        });
-    });
-});
 
 document.addEventListener("DOMContentLoaded", function() {
     // Function to show loader and hide button
@@ -68,14 +42,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // Handle updateForm submission
     document.getElementById("updateForm").addEventListener("submit", function(event) {
       showLoader(document.getElementById("update_button"), "loader");
-    });
-  
-    // Handle deleteStockForm submission
-    document.querySelectorAll(".deleteStockForm").forEach(function(form) {
-      form.addEventListener("submit", function(event) {
-        const button = form.querySelector("button");
-        showLoader(button, "loader");
-      });
     });
   });
   
